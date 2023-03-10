682c62614f049101dbcc2d56a0ee2770f2b1b215
Kenneth <kenneth.ho@tnis.com>
2023-03-10 13:20:09 +0700

    feature-2: add feature queue

--- feature-2/queue.go
+++ feature-2/queue.go
@@ -1,1 +1,35 @@
 package feature_2
+
+import "errors"
+
+type Queue struct {
+	items []int
+	size  int
+}
+
+func Construct(maxSize int) Queue {
+	qItems := make([]int, 0, maxSize)
+	return Queue{qItems, 0}
+}
+
+func (q *Queue) Enqueue(item int) {
+	q.items = append(q.items, item)
+	q.size++
+}
+
+func (q *Queue) Dequeue() (int, error) {
+	if q.size == 0 {
+		return -1, errors.New("empty queue")
+	}
+	item := q.items[0]
+	q.items = q.items[1:]
+	q.size--
+	return item, nil
+}
+
+func (q *Queue) Peek() (int, error) {
+	if q.size == 0 {
+		return -1, errors.New("empty queue")
+	}
+	return q.items[0], nil
+}
